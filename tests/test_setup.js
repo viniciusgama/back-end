3313879973f4c3ff8e33d1c23b7b5318d849f0ea
Vinicius Gama <vinicius.sgama@gmail.com>
2016-05-14 17:25:15 -0300

    #30 @viniciusgama - changes test_setup create the data in db depending on NODE_ENV

--- tests/test_setup.js
+++ tests/test_setup.js
@@ -1,13 +1,66 @@
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost/vamosjuntas_test',function(error, db) {
-  if (error){
+var enviroment = process.env.NODE_ENV || 'test';
+var connectionURL = {
+  test: 'mongodb://localhost/vamosjuntas_test',
+  app: 'mongodb://localhost/vamosjuntas'
+};
+
+MongoClient.connect(connectionURL[enviroment], function(error, db) {
+  if (error) {
     console.log('error mongo');
   } else {
-    var places = [{loc:['-30.060653, -51.171270'],address: 'Rua blá',
-    reports:[{category:'local deserto', date:'2016-03-16T12:41:51.002Z'}]}];
+    var places = [{
+      loc: [-51.196491, -30.042510],
+      address: 'Rua 1',
+      reports: [{
+        category: 'local deserto',
+        date: '2016-03-16T12:41:51.002Z'
+      }]
+    },
+    {
+      loc: [-51.209619, -30.059620],
+      address: 'Rua 2',
+      reports: [{
+        category: 'local deserto',
+        date: '2016-03-16T12:41:51.002Z'
+      }]
+    },
+    {
+      loc: [-51.230576, -30.072586],
+      address: 'Rua 3',
+      reports: [{
+        category: 'local deserto',
+        date: '2016-03-16T12:41:51.002Z'
+      }]
+    },
+    {
+      loc: [-51.195136, -30.017054],
+      address: 'Rua 4',
+      reports: [{
+        category: 'local deserto',
+        date: '2016-03-16T12:41:51.002Z'
+      }]
+    },
+    {
+      loc: [-49.549665, -28.688702],
+      address: 'Rua 5',
+      reports: [{
+        category: 'local deserto',
+        date: '2016-03-16T12:41:51.002Z'
+      }]
+    },
+    {
+      loc: [-51.169215, -30.056248],
+      address: 'Av Cristiano Fischer',
+      reports: [{
+        category: 'local deserto',
+        date: '2016-03-16T12:41:51.002Z'
+      }]
+    }];
+
     db.collection('places').remove(function() {
-      db.collection('places').insert(places,function() {
+      db.collection('places').insert(places, function() {
         db.close();
       });
     });
